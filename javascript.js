8af5e076adb73bc457fad5095ce2aa661a62175f
is709851 <is709851@iteso.mx>
2022-10-05 20:23:55 -0500

    version video

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
-function video() {
+function cancion() {
     var x = document.getElementById("cancion");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+}
+function video() {
+    var x = document.getElementById("video");
         if (x.style.display === "none") {
             x.style.display = "block";
         } else {
